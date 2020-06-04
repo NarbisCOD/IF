6be687180e8597efc4bb54a873c21bd8709a1962
Narbis Tamiche <54629185+NarbisCOD@users.noreply.github.com>
2020-06-04 00:07:09 -0300

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -19,6 +19,9 @@ int main(){
 
     printf("Ingrese su Clave:\n");
     scanf("%i", &clave_ingresada);
+    
+    //int strcmp ( const char * str1, const char * str2 );
+    //Si el valor de retorno es igual a 0 indica que ambas cadenas son iguales (str1 == str2)
     if (((strcmp(usuario, usuario_1)==0) && (clave_ingresada == CLAVE_1)) || ((strcmp(usuario, usuario_2)==0) && (clave_ingresada == CLAVE_2)) || ((strcmp(usuario, usuario_3)==0) && (clave_ingresada == CLAVE_3))){
         printf("Bienvenido: %s", usuario);
     }else{
